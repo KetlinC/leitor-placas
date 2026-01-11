1d4176da3d31448f8a6157e831b7391e830f8865
Ketlin Castro de Sousa <ketlin.sousa@centenario.com.br>
2026-01-11 17:57:46 -0300

    Semana 2: pré-processamento de imagem (cinza, blur e bordas)

--- src/main.py
+++ src/main.py
@@ -6,7 +6,22 @@ imagem = cv2.imread("images/exemplo_placa.jpg")
 # Verificar se carregou
 if imagem is None:
     print("Erro ao carregar a imagem")
-else:
-    cv2.imshow("Imagem", imagem)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    exit()
+
+# Converter para cinza
+cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+
+#Aplicando Blur
+blur = cv2.GaussianBlur(cinza, (5,5), 0)
+
+#Detecção de bordas
+bordas = cv2.Canny(blur, 100, 200)
+
+cv2.imshow("Original", imagem)
+cv2.imshow("Cinza", cinza)
+cv2.imshow("Blur", blur)
+cv2.imshow("Bordas", bordas)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
